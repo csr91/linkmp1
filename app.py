1ef8ee9ed3ada67da396f5a8a4a7ebdb01d82423
Cesar <cesar.mendoza.77@gmail.com>
2023-12-08 19:55:24 -0300

    wh

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ tokens_mapping = {
     "6000": os.environ.get('MERCADO_PAGO_BUPE_1C')
 }
 
-def linkmp(payload, access_token):
+def linkmp(payload, access_token, webhook_url):
     org_vta = payload.get("org_vta")
     access_token = tokens_mapping.get(org_vta)
 
@@ -44,10 +44,15 @@ def linkmp(payload, access_token):
                 "quantity": 1,
                 "unit_price": total_amount,
             }
-        ]
+        ],
+        "notification_url": webhook_url  # Agregar la URL del webhook aquí
     }
 
     preference_response = sdk.preference().create(preference_data)
+
+    # Imprimir la respuesta de MercadoPago en la terminal
+    print("Respuesta de MercadoPago:")
+    print(preference_response)
     
     payment_link = preference_response['response'].get('init_point', '')
 
@@ -56,6 +61,7 @@ def linkmp(payload, access_token):
         return {'preference_id': preference_id, 'enlace_de_pago': payment_link}
     else:
         return {'error': 'La respuesta no contiene el ID de preferencia', 'enlace_de_pago': payment_link}, 500
+
 
 @app.route('/')
 def index():
@@ -79,11 +85,9 @@ def login():
     return jsonify({'access_token': access_token})
 
 
+
 @app.route('/generar_link', methods=['POST'])
 def generar_link():
-    # print(request.method)
-    print(request.headers)
-    # print(request.get_data())
     access_token = request.headers.get('Authorization')
 
     if access_token is None or not access_token.startswith('Bearer '):
@@ -98,10 +102,29 @@ def generar_link():
         if payload is None or 'org_vta' not in payload:
             return jsonify({'error': 'No se proporcionó un JSON válido o falta org_vta'}), 400
 
-        payment_data = linkmp(payload, access_token)
+        webhook_url = "http://127.0.0.1:5000/webhook"  # Reemplaza con la URL de tu webhook
+        payment_data = linkmp(payload, access_token, webhook_url)
         return jsonify(payment_data)
     except:
         return jsonify({'error': 'Token inválido'}), 401
+
+    
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    try:
+        # Verificar si la solicitud tiene un cuerpo JSON
+        if request.is_json:
+            data = request.json
+            # Imprimir el cuerpo del webhook en la terminal
+            print("Webhook Data:")
+            print(data)
+            # Aquí puedes agregar la lógica para procesar los datos del webhook según tus necesidades
+
+            return jsonify({'status': 'ok'}), 200
+        else:
+            return jsonify({'error': 'La solicitud no contiene datos JSON'}), 400
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
