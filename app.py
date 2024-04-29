c12f841b336cc3b098ddd14604597134224803f0
Cesar <cmendoza@jobint.com>
2024-04-29 12:00:58 -0300

    1

--- app.py
+++ app.py
@@ -8,5 +8,18 @@ app = Flask(__name__)
 def hola():
     return 'hola'
     
+# URI de conexión a MongoDB
+uri = "mongodb+srv://cesarmendoza77:7hLCBopqFoTBmF4v@cluster0.papc1wn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Crear un nuevo cliente y conectar al servidor
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Enviar un ping para confirmar una conexión exitosa
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
 if __name__ == '__main__':
     app.run(debug=True)
